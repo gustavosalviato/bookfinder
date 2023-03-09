25f61381dc4f3b9c56b6444e3c1ff0279c357d1d
gustavosalviato <gustavosalviato8@gmail.com>
2023-03-09 19:12:07 +0100

    chore: adjusting some styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
+import Image from "next/image";
 
-export default function Home(){
+export default function Home() {
   return (
-    <div className="min-h-screen bg-background flex flex-col">
-      <Header/>
-    </div>
-  )
+    <>
+      <div className="h-screen w-screen flex flex-col">
+        <Header />
+
+        <main className="flex items-center justify-center max-w-[1120px] mx-auto h-full mt-20 mb-20 px-4 max-lg:flex-col max-sm:px-8">
+          <Hero />
+
+          <img
+            className="w-[570px] h-[420px] max-lg:w-[500px] max-lg:h-[500px] mt-8 max-sm:hidden"
+            src="/images/reading.svg"
+            alt=""
+          />
+        </main>
+      </div>
+    </>
+  );
 }
