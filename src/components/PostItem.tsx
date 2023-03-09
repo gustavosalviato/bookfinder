d9df4f0c395e675fe91e44b7a6f3f994d2006b87
gustavosalviato <gustavosalviato8@gmail.com>
2023-03-09 20:31:59 +0100

    chore: PostItem component

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+interface PostProps {
+  pusblishDate: string;
+  title: string;
+  description: string;
+
+}
+
+export function PostItem({ pusblishDate, description, title }: PostProps) {
+  return (
+    <section className="flex w-full flex-col border-b border-secondary mb-8 pb-8">
+      <data className="text-secondary">{pusblishDate}</data>
+
+      <Link
+        href="/books/1"
+        className="mt-4 text-headline transition-colors hover:text-highlight hover:underline"
+      >
+        <strong className="text-2xl  font-bold">{title}</strong>
+      </Link>
+
+      <p className="text-paragraph mt-2 text-lg leading-relaxed text-justify">
+        {description}
+      </p>
+    </section>
+  );
+}
