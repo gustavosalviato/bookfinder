d2f7dcfb39cced3ff16eb7f62b3e089d4ab5d58e
gustavosalviato <gustavosalviato8@gmail.com>
2023-03-10 15:32:36 +0100

    chore: adding props to PostItem

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -4,16 +4,21 @@ interface PostProps {
   pusblishDate: string;
   title: string;
   description: string;
-
+  slug: string;
 }
 
-export function PostItem({ pusblishDate, description, title }: PostProps) {
+export function PostItem({
+  pusblishDate,
+  description,
+  title,
+  slug,
+}: PostProps) {
   return (
     <section className="flex w-full flex-col border-b border-secondary mb-8 pb-8">
       <data className="text-secondary">{pusblishDate}</data>
 
       <Link
-        href="/books/1"
+        href={`/books/${slug}`}
         className="mt-4 text-headline transition-colors hover:text-highlight hover:underline"
       >
         <strong className="text-2xl  font-bold">{title}</strong>
